6ea8af568c6d5f51f04e4e72916e16f6aa4c176b
JohnnyP <wabanichi@gmail.com>
2019-04-24 19:10:34 -0500

    added routing through app.jsx and root.jsx

--- frontend/scoreboard.jsx
+++ frontend/scoreboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	window.getState = store.getState;
 	window.dispatch = store.dispatch;
+
 	const root = document.getElementById('root');
-	ReactDOM.render(<h1>I DECLARE BANKRUPTCY!</h1>, root);
+	ReactDOM.render(<Root store={store} />, root);
 });
