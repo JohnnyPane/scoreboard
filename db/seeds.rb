9da7c1a365b48de9dacfa49fc2126cdd1223fc50
JohnnyP <wabanichi@gmail.com>
2019-06-18 14:11:08 -0500

    add new seed data and confirms for deleting scores

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,46 +5,156 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Score.delete_all;
-Standing.delete_all;
+# Score.delete_all;
+# Standing.delete_all;
 
-Score.create!(
-	game_type: 'Ping Pong',
-	player_one: 'Coleman',
-	player_two: 'JP',
-	author_id: 1,
-	final_score: 100,
-	player_one_score: 100,
-	player_two_score: 97
-);
+# Score.create!(
+# 	game_type: 'Ping Pong',
+# 	player_one: 'guest',
+# 	player_two: 'ForrestGump',
+# 	author_id: 4,
+# 	final_score: 21,
+# 	player_one_score: 17,
+# 	player_two_score: 21
+# );
 
-Score.create!(
-	game_type: 'Ping Pong',
-	player_one: 'Coleman',
-	player_two: 'JP',
-	author_id: 1,
-	final_score: 23,
-	player_one_score: 23,
-	player_two_score: 21
-);
+# Score.create!(
+# 	game_type: 'Ping Pong',
+# 	player_one: 'guest',
+# 	player_two: 'ForrestGump',
+# 	author_id: 4,
+# 	final_score: 21,
+# 	player_one_score: 21,
+# 	player_two_score: 17
+# );
 
-Score.create!(
-	game_type: 'Ping Pong',
-	player_one: 'Coleman',
-	player_two: 'JP',
-	author_id: 1,
-	final_score: 87,
-	player_one_score: 77,
-	player_two_score: 87
-);
+# Score.create!(
+# 	game_type: 'Ping Pong',
+# 	player_one: 'guest',
+# 	player_two: 'ForrestGump',
+# 	author_id: 4,
+# 	final_score: 21,
+# 	player_one_score: 13,
+# 	player_two_score: 21
+# );
 
-Score.create!(
-	game_type: 'Ping Pong',
-	player_one: 'Coleman',
-	player_two: 'JP',
-	author_id: 1,
-	final_score: 45,
-	player_one_score: 45,
-	player_two_score: 37
-);
+# Score.create!(
+# 	game_type: 'Ping Pong',
+# 	player_one: 'guest',
+# 	player_two: 'ForrestGump',
+# 	author_id: 4,
+# 	final_score: 21,
+# 	player_one_score: 19,
+# 	player_two_score: 21
+# );
+
+# Score.create!(
+# 	game_type: 'Ping Pong',
+# 	player_one: 'guest',
+# 	player_two: 'ForrestGump',
+# 	author_id: 4,
+# 	final_score: 21,
+# 	player_one_score: 21,
+# 	player_two_score: 3
+# );
+
+# Score.create!(
+# 	game_type: 'Darts',
+# 	player_one: 'guest',
+# 	player_two: 'Bullwinkle',
+# 	author_id: 4,
+# 	final_score: 301,
+# 	player_one_score: 301,
+# 	player_two_score: 287
+# );
+
+# Score.create!(
+# 	game_type: 'Darts',
+# 	player_one: 'guest',
+# 	player_two: 'Bullwinkle',
+# 	author_id: 4,
+# 	final_score: 301,
+# 	player_one_score: 301,
+# 	player_two_score: 297
+# );
+
+# Score.create!(
+# 	game_type: 'Darts',
+# 	player_one: 'guest',
+# 	player_two: 'Bullwinkle',
+# 	author_id: 4,
+# 	final_score: 301,
+# 	player_one_score: 247,
+# 	player_two_score: 301
+# );
+
+# Score.create!(
+# 	game_type: 'Darts',
+# 	player_one: 'guest',
+# 	player_two: 'Bullwinkle',
+# 	author_id: 4,
+# 	final_score: 301,
+# 	player_one_score: 301,
+# 	player_two_score: 291
+# );
+
+# Score.create!(
+# 	game_type: 'Darts',
+# 	player_one: 'guest',
+# 	player_two: 'Bullwinkle',
+# 	author_id: 4,
+# 	final_score: 301,
+# 	player_one_score: 301,
+# 	player_two_score: 177
+# );
+
+# Score.create!(
+# 	game_type: 'Foosball',
+# 	player_one: 'guest',
+# 	player_two: 'Bobby',
+# 	author_id: 4,
+# 	final_score: 10,
+# 	player_one_score: 2,
+# 	player_two_score: 10
+# );
+
+# Score.create!(
+# 	game_type: 'Foosball',
+# 	player_one: 'guest',
+# 	player_two: 'Bobby',
+# 	author_id: 4,
+# 	final_score: 10,
+# 	player_one_score: 4,
+# 	player_two_score: 10
+# );
+
+# Score.create!(
+# 	game_type: 'Foosball',
+# 	player_one: 'guest',
+# 	player_two: 'Bobby',
+# 	author_id: 4,
+# 	final_score: 10,
+# 	player_one_score: 6,
+# 	player_two_score: 10
+# );
+
+# Score.create!(
+# 	game_type: 'Foosball',
+# 	player_one: 'guest',
+# 	player_two: 'Bobby',
+# 	author_id: 4,
+# 	final_score: 10,
+# 	player_one_score: 8,
+# 	player_two_score: 10
+# );
+
+# Score.create!(
+# 	game_type: 'Foosball',
+# 	player_one: 'guest',
+# 	player_two: 'Bobby',
+# 	author_id: 4,
+# 	final_score: 10,
+# 	player_one_score: 10,
+# 	player_two_score: 8
+# );
 
--- frontend/components/home/score_index_item.jsx
+++ frontend/components/home/score_index_item.jsx
@@ -6,12 +6,17 @@ class ScoreIndexItem extends React.Component {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
 
   handleDelete(e) {
   	e.stopPropagation();
   	const scoreId = this.props.score.id;
   	this.props.destroyScore(scoreId);
+  }
+
+  handleCancel(e) {
+  	e.stopPropagation();
   }
 
   handleClick() {
@@ -23,6 +28,7 @@ class ScoreIndexItem extends React.Component {
 
 	render() {
 		const { game_type, player_one, player_two, final_score, player_one_score, player_two_score } = this.props.score;
+
 		return (
 			<div className="score-index-item"
 					 onClick={this.handleClick} >
@@ -34,7 +40,7 @@ class ScoreIndexItem extends React.Component {
 					<p>final score: {final_score}</p>
 				</li>
 				<div className="delete-button-wrapper">
-					<button className="delete-button" onClick={ this.handleDelete }>
+					<button className="delete-button" onClick={(e) => { if (window.confirm('Are you sure you wish to delete this item?')) { this.handleDelete(e) } else { this.handleCancel(e) } } }>
 	          Delete Score
 	        </button>
         </div>
